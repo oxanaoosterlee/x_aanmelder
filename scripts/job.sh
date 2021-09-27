29f77bf6116105941be5b8961a513c5dae880821
Oxana <oxanaoosterlee@gmail.com>
2021-09-27 14:13:40 +0200

    Update job.sh script to include VENV directory.

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-gnome-terminal -e "/bin/bash -c '/home/oxana/Documents/x_aanmelder/x_aanmelder/x_aanmelder.py; exec /bin/bash -i'"
+BASH_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+LIBRARY_DIR="$(dirname "$BASH_SCRIPT_DIR")"
+VENV_SCRIPT_DIR="$LIBRARY_DIR/venv/bin/python3" 
+SCRIPT_DIR="$LIBRARY_DIR/xaanmelder" 
+echo $LIBRARY_DIR
+echo $VENV_SCRIPT_DIR
+echo $SCRIPT_DIR
+gnome-terminal -- $VENV_SCRIPT_DIR $SCRIPT_DIR/x_aanmelder.py
